a524c9d65d1a5b4dee1635769a70ffd9bf383414
Nischay Sharma <sharmanischay23@gmail.com>
2020-10-16 11:54:51 +0530

    added database access

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
 // load the things we need
-var express = require('express');
+var express = require("express");
 var app = express();
+var monk = require("monk");
+var db = monk("localhost:27017/testdb");
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use( express.static( 'public' ) );
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-// use res.render to load up an ejs view file
-
-// index page
-// app.get('/', function(req, res) {
-//     res.render('pages/main');
-// });
+app.use(function (req, res, next) {
+  req.db = db;
+  next();
+});
 
 
-app.get('/', function(req, res) {
-    var images = [];
-    let names;
-    for(let i=1;i<=10;i++)
-    {
-        name=i+".jpg";
-        images.push(name);
-    }
-    var tagline = "No programming concept is complete without a cute animal mascot.";
-    console.log(images);
-    res.render('./pages/main', {
-        images:images,
-        tagline: tagline
+app.get("/", function (req, res) {
+  var db = req.db;
+  var collection = db.get("bio");
+  collection.find({},  function (e, data) {
+    res.render("./pages/main", {
+      biodata: data,
     });
+  });
 });
 
 app.listen(8080);
